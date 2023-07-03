64a2f860e2393d981331d5f76d50064919d7194a
Andrey20023 <66505241+Andrey20023@users.noreply.github.com>
2023-07-03 23:49:19 +0300

    Add new

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,47 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         StringBuffer strBuffer = new StringBuffer("Start of log:\n");
-        myMkDir(strBuffer, "C:\\Games\\", "src");
-        myMkDir(strBuffer, "C:\\Games\\", "res");
-        myMkDir(strBuffer, "C:\\Games\\", "savegames");
-        myMkDir(strBuffer, "C:\\Games\\", "temp");
-        myMkDir(strBuffer, "C:\\Games\\src", "main");
-        myMkDir(strBuffer, "C:\\Games\\src", "test");
-        myCreateNewFile(strBuffer, "C:\\Games\\src\\main\\", "Main.java");
-        myCreateNewFile(strBuffer, "C:\\Games\\src\\main\\", "Utils.java");
-        myMkDir(strBuffer, "C:\\Games\\res\\", "drawables");
-        myMkDir(strBuffer, "C:\\Games\\res\\", "vectors");
-        myMkDir(strBuffer, "C:\\Games\\res\\", "icons");
-        myCreateNewFile(strBuffer, "C:\\Games\\temp\\", "temp.txt");
-        try (FileWriter writer = new FileWriter("C:\\Games\\temp\\temp.txt", true)) {
-            String text = "Hello Gold!";
+        List<String> listPath = Arrays.asList(
+                "C:\\Games\\src", "C:\\Games\\res",
+                "C:\\Games\\savegames", "C:\\Games\\temp",
+                "C:\\Games\\src\\main", "C:\\Games\\src\\test",
+                "C:\\Games\\res\\drawables", "C:\\Games\\res\\vectors",
+                "C:\\Games\\res\\icons");
+        for (String arr : listPath) {
+            File nameDir = new File(arr);
+            strBuffer.append(nameDir.mkdir() ? "Каталог " + arr + " создан.\n" : "Каталог " + arr + " не создан.\n");
+        }
+
+        strBuffer.append(
+                myCreateNewFile("C:\\Games\\src\\main\\", "Main.java") +
+                        myCreateNewFile("C:\\Games\\src\\main\\", "Utils.java") +
+                        myCreateNewFile("C:\\Games\\temp\\", "temp.txt"));
+
+        try (
+                FileWriter writer = new FileWriter("C:\\Games\\temp\\temp.txt", true)) {
             writer.write(String.valueOf(strBuffer));
             writer.append("Еnd of log.\n");
             writer.flush();
-        } catch (IOException ex) {
+        } catch (
+                IOException ex) {
             System.out.println(ex.getMessage());
         }
+
     }
 
-    public static void myMkDir(StringBuffer strBuffer, String path, String name) {
-        File nameDir = new File(path, name);
-        if (nameDir.mkdir()) {
-            strBuffer.append("Каталог " + name + " создан.\n");
-        } else {
-            strBuffer.append("Каталог " + name + " не создан.\n");
-        }
-    }
-
-    public static void myCreateNewFile(StringBuffer strBuffer, String path, String name) {
+    public static String myCreateNewFile(String path, String name) {
         File myFile = new File(path, name);
         try {
-            if (myFile.createNewFile()) {
-                strBuffer.append("Файл " + name + " создан.\n");
-            } else {
-                strBuffer.append("Файл " + name + " не создан.\n");
-            }
+            return myFile.createNewFile() ? "Файл " + path + name + " создан.\n" : "Файл " + path + name + " не создан.\n";
         } catch (IOException ex) {
-            strBuffer.append("Файл не создан, ошибка IOException: " + ex.getMessage() + "\n");
+            return "Файл не создан, ошибка IOException: " + ex.getMessage() + "\n";
         }
     }
 }
